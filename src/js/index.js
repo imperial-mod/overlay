1737a96d2f11ddd4d93d1195c384e80ebadb2b42
zxnii <61130784+Zxnii@users.noreply.github.com>
2021-05-08 14:29:08 -0600

    add multi-game support

--- src/js/index.js
+++ src/js/index.js
@@ -23,24 +23,7 @@ const LogReader = require("./js/LogReader")
 const DiscordRPC = require("discord-rpc")
 const package = require("../package.json")
 
-const colors = {
-    "DARK_RED": "#AA0000",
-    "RED": "#FF5555",
-    "GOLD": "#FFAA00",
-    "YELLOW": "#FFFF55",
-    "DARK_GREEN": "#00AA00",
-    "GREEN": "#55FF55",
-    "AQUA": "#55FFFF",
-    "DARK_AQUA": "#00AAAA",
-    "DARK_BLUE": "#0000AA",
-    "BLUE": "#5555FF",
-    "LIGHT_PURPLE": "#FF55FF",
-    "DARK_PURPLE": "#AA00AA",
-    "WHITE": "#FFFFFF",
-    "GRAY": "#AAAAAA",
-    "DARK_GRAY": "#555555",
-    "BLACK": "#000000"
-}
+const colors = require("./constants.json").colors
 
 const ranks = {
     "ADMIN": `<span style="color: ${colors["RED"]}">[ADMIN] `,
@@ -55,19 +38,9 @@ const ranks = {
     "NON": `<span style="color: ${colors["GRAY"]}">`
 }
 
-const threatColors = [
-    "GREEN",
-    "YELLOW",
-    "RED",
-    "DARK_PURPLE"
-]
+const threatColors = require("./constants.json").threatColors
 
-const threatNames = [
-    "LOW",
-    "MEDIUM",
-    "HIGH",
-    "EXTREME"
-]
+const threatNames = require("./constants.json").threatNames
 
 window.addEventListener("load", () => {
     const userList = document.querySelector("#users")
@@ -84,6 +57,11 @@ window.addEventListener("load", () => {
     let changedLogs = []
     let logs = []
     let users = []
+    let mode = null
+    let game = null
+    let statsFunction = () => {
+        return []
+    }
 
     let config = {
         user: "",
@@ -102,7 +80,7 @@ window.addEventListener("load", () => {
         hypixelApi = new api.HypixelAPI(config.apiKey)
         logPath = path.join(config.minecraftPath, "latest.log")
 
-        const logReader = new LogReader(logPath)
+        const logReader = new LogReader(logPath, config.user)
 
         logReader.on("server_change", () => {
             users = []
@@ -118,110 +96,65 @@ window.addEventListener("load", () => {
                 hypixelApi.getPlayer(uuid).then(async (res) => {
                     const player = res.player
 
-                    let mode = null
-
                     if (name == config.user) {
                         const status = await hypixelApi.getStatus(uuid)
 
-                        mode = status.session.mode.split("_")[0].toLowerCase()
+                        game = status.session.gameType
+                        mode = status.session.mode
+
+                        console.log(path.join(__dirname, `/js/games/${game.toLowerCase()}.js`))
+
+                        if (fs.existsSync(path.join(__dirname, `/js/games/${game.toLowerCase()}.js`))) {
+                            const statFile = require(`./js/games/${game.toLowerCase()}`)
+                            const usersHeader = document.querySelector("#users-header")
+                            const userHeaderCatergories = document.querySelectorAll("#users-header > *")
+                            const statCategories = ["Name"]
+
+                            statCategories.push(...statFile.stats)
+
+                            console.log(statCategories)
+                            console.log(statFile)
+
+                            statsFunction = statFile.getStats
+
+                            for (const element of userHeaderCatergories) {
+                                element.remove()
+                            }
+
+                            for (const category of statCategories) {
+                                const element = document.createElement("td")
+                                element.innerText = category
+                                usersHeader.appendChild(element)
+                            }
+                        } else {
+                            const usersHeader = document.querySelector("#users-header")
+                            const userHeaderCatergories = document.querySelectorAll("#users-header > *")
+                            const statCategories = ["Name"]
+
+                            for (const element of userHeaderCatergories) {
+                                element.remove()
+                            }
+
+                            for (const category of statCategories) {
+                                const element = document.createElement("td")
+                                element.innerText = category
+                                usersHeader.appendChild(element)
+                            }
+
+                            statsFunction = () => {
+                                return []
+                            }
+                        }
                     }
                     console.log(mode)
                     console.log(player)
-                    const guild = await hypixelApi.getGuild(uuid)
 
                     const userElement = document.createElement("tr")
-
-                    const threatElement = document.createElement("td")
                     const nameElement = document.createElement("td")
-                    const wlrElement = document.createElement("td")
-                    const kdrElement = document.createElement("td")
-                    const bwsElement = document.createElement("td")
-                    const wsElement = document.createElement("td")
-                    const winElement = document.createElement("td")
-                    const lossElement = document.createElement("td")
-                    const aimElement = document.createElement("td")
 
-                    console.log(guild)
-
-                    if (player && player.stats["Duels"]) {
-                        const wins = player.stats["Duels"][`${mode ? mode+"_duel_" : ""}wins`] || 0
-                        const losses = player.stats["Duels"][`${mode ? mode+"_duel_" : ""}losses`] || 0
-                        const bws = player.stats["Duels"][`best_${mode || "overall"}_winstreak`] || 0
-                        const ws = player.stats["Duels"][`current_${mode ? mode+"_" : ""}winstreak`] || 0
-                        const wlr = Math.round((wins / losses) * 100) / 100 || wins
-                        const kdr = Math.round((player.stats["Duels"][`${mode ? mode+"_duel_" : ""}kills`] / player.stats["Duels"][`${mode ? mode+"_duel_" : ""}deaths`]) * 100) / 100 || player.stats["Duels"]["kills"]
-                        const aim = Math.round(((player.stats["Duels"]["melee_hits"] / player.stats["Duels"]["melee_swings"])*100)*100) / 100
-
-                        let threatLevel = 0
-                        let wlrThreat = 0
-                        let kdrThreat = 0
-                        let wsThreat = 0
-                        let bwsThreat = 0
-                        let aimThreat = 0
-
-                        console.log(player.stats["Duels"])
-
+                    if (player) {
+                        const guild = await hypixelApi.getGuild(uuid)
                         const rank = player.rank || (player.monthlyPackageRank == "SUPERSTAR" ? "SUPERSTAR" : undefined || player.newPackageRank || "NON")
-
-                        console.log(rank)
-                        
-                        if (wlr >= 15) {
-                            threatLevel += 3
-                            wlrThreat = 3
-                        } else if (wlr >= 5) {
-                            threatLevel += 2
-                            wlrThreat = 2
-                        } else if (wlr >= 2.5) {
-                            threatLevel++
-                            wlrThreat = 1
-                        }
-
-                        if (kdr >= 15) {
-                            threatLevel += 3
-                            kdrThreat = 3
-                        } else if (kdr >= 10) {
-                            threatLevel += 2
-                            kdrThreat = 2
-                        } else if (kdr >= 3) {
-                            threatLevel++
-                            kdrThreat = 1
-                        }
-
-                        if (ws >= 150) {
-                            threatLevel += 3
-                            wsThreat = 3
-                        } else if (ws >= 50) {
-                            threatLevel += 2
-                            wsThreat = 2
-                        } else if (ws >= 15) {
-                            threatLevel++
-                            wsThreat = 1
-                        }
-
-                        if (bws >= 150) {
-                            threatLevel += 3
-                            bwsThreat = 3
-                        } else if (bws >= 50) {
-                            threatLevel += 2
-                            bwsThreat = 2
-                        } else if (bws >= 20) {
-                            threatLevel++
-                            bwsThreat = 1
-                        }
-
-                        if (aim >= 70) {
-                            threatLevel += 3
-                            aimThreat = 3
-                        } else if (aim >= 60) {
-                            threatLevel += 2
-                            aimThreat = 2
-                        } else if (aim >= 40) {
-                            threatLevel += 1
-                            aimThreat = 1
-                        }
-
-                        const overallThreatLevel = Math.round(threatLevel/5)
-                        nameElement.innerHTML = ""
 
                         if (config.youTag && name == config.user) {
                             nameElement.innerHTML = `<span style="color: ${colors.AQUA};">[Y]</span> `
@@ -231,27 +164,21 @@ window.addEventListener("load", () => {
                         if (guild && guild.tag) {
                             nameElement.innerHTML += ` <span style="color: ${colors[guild.tagColor] || colors["GRAY"]};">[${guild.tag}]</span>`
                         }
-                        threatElement.innerHTML = `<span style="color: ${colors[threatColors[overallThreatLevel]]}">${threatNames[overallThreatLevel]}</span>`
-                        wlrElement.innerHTML = `<span style="color: ${colors[threatColors[wlrThreat]]};">${numberFormatter.format(wlr)}</span>` || "N/A"
-                        kdrElement.innerHTML = `<span style="color: ${colors[threatColors[kdrThreat]]};">${numberFormatter.format(kdr)}</span>` || "N/A"
-                        bwsElement.innerHTML = `<span style="color: ${colors[threatColors[bwsThreat]]};">${numberFormatter.format(bws)}</span>` || "N/A"
-                        wsElement.innerHTML = `<span style="color: ${colors[threatColors[wsThreat]]};">${numberFormatter.format(ws)}</span>` || "N/A"
-                        winElement.innerHTML = `<span style="color: ${colors["GRAY"]};">${numberFormatter.format(wins)}</span>`
-                        lossElement.innerHTML = `<span style="color: ${colors["GRAY"]};">${numberFormatter.format(losses)}</span>`
-                        aimElement.innerHTML = `<span style="color: ${colors[threatColors[aimThreat]]};">${aim}%</span>` || "N/A"
+
+                        const stats = statsFunction(player, mode, numberFormatter)
+
+                        console.log(stats)
+
+                        userElement.append(nameElement)
+
+                        for (const stat of stats) {
+                            const element = document.createElement("td")
+                            element.innerHTML = stat
+                            userElement.append(element)
+                        }
                     } else {
                         nameElement.innerHTML = `<span style="color: ${colors["RED"]};">${name} (NICKED)</span>`
                     }
-
-                    userElement.append(threatElement)
-                    userElement.append(nameElement)
-                    userElement.append(wlrElement)
-                    userElement.append(kdrElement)
-                    userElement.append(aimElement)
-                    userElement.append(bwsElement)
-                    userElement.append(wsElement)
-                    userElement.append(winElement)
-                    userElement.append(lossElement)
 
                     userElement.className = "user"
                     userElement.id = `user-${name}`
@@ -273,39 +200,37 @@ window.addEventListener("load", () => {
 
         const setRpc = async () => {
             const uuid = await mcApi.getUuid(config.user)
-            const playerRes = await hypixelApi.getPlayer(uuid)
-            const player = playerRes.player
 
-            if (player.stats["Duels"]) {
-                const ws = player.stats["Duels"]["current_winstreak"] || 0
-                const status = await hypixelApi.getStatus(uuid)
+            const status = await hypixelApi.getStatus(uuid)
 
-                console.log(status.session)
+            console.log(status.session)
 
-                if (status.session.online && status.session.gameType == "DUELS") {
-                    if (status.session.mode == "LOBBY") {
-                        rpc.setActivity({
-                            details: "In a Duels Lobby",
-                            startTimestamp,
-                            state: `Winstreak: ${ws}`,
-                            largeImageKey: "icon",
-                            largeImageText: `Duels Overlay | v${package.version}`,
-                            instance: false
-                        })
-                    } else {
-                        let mode = status.session.mode.split("_")[0].toLowerCase()
+            if (status.session.online) {
+                let gameChunks = status.session.gameType.split("_")
+                let game = ""
 
-                        mode = mode[0].toUpperCase() + mode.slice(1)
+                for (let chunk of gameChunks) {
+                    console.log(chunk)
+                    if (chunk)
+                        game += chunk[0] + chunk.slice(1).toLowerCase() + " "
+                }
 
-                        rpc.setActivity({
-                            details: `In a ${mode} Duel`,
-                            startTimestamp,
-                            state: `Winstreak: ${ws}`,
-                            largeImageKey: "icon",
-                            largeImageText: `Duels Overlay | v${package.version}`,
-                            instance: false
-                        })
-                    }
+                if (status.session.mode == "LOBBY") {
+                    rpc.setActivity({
+                        details: `In a ${game.trim()} Lobby`,
+                        startTimestamp,
+                        largeImageKey: "icon",
+                        largeImageText: `Duels Overlay | v${package.version}`,
+                        instance: false
+                    })
+                } else {
+                    rpc.setActivity({
+                        details: `Playing ${game.trim()}`,
+                        startTimestamp,
+                        largeImageKey: "icon",
+                        largeImageText: `Duels Overlay | v${package.version}`,
+                        instance: false
+                    })
                 }
             }
         }
