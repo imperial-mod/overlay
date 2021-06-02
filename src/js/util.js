5e4b3146030d953e104c09c76f08e372421de465
zxnii <61130784+Zxnii@users.noreply.github.com>
2021-06-01 22:54:20 -0600

    add license

--- src/js/util.js
+++ src/js/util.js
@@ -1,4 +1,22 @@
+/*
+	Copyright (C) 2021  zani
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 const colors = require("../constants.json").colors
+const rankedDivisions = require("../constants.json").rankedDivisions
 
 const clamp = (inp, min, max) => {
     if (inp > max)
@@ -31,7 +49,7 @@ module.exports = {
             console.log(patternPiece)
             if ((char == "[" || char == "]") && prestige.bracket)
                 finalString += `<span style="color: ${colors[prestige.bracket.toUpperCase()]}">${char}</span>`
-            else if (char == prestige.star && prestige.starColor)
+            else if (char == star && prestige.starColor)
                 finalString += `<span style="color: ${colors[prestige.starColor.toUpperCase()]}">${char}</span>`
             else {
                 finalString += `<span style="color: ${colors[color.toUpperCase()]}">${char}</span>`
@@ -52,5 +70,19 @@ module.exports = {
             }
         }
         return finalString
-    }
+    },
+	getDivision: (position = 50001) => {
+		let lastDiv = rankedDivisions[0]
+		for (const division of rankedDivisions) {
+			if (division.position < position) {
+				return lastDiv
+			}
+			lastDiv = division
+		}
+		if (position == 0) return rankedDivisions[0]
+		return lastDiv
+	},
+	stripFormatting: (string) => {
+		return string.replace(/§./g, "")
+	}
 }
